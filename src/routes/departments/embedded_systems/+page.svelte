b7cad31d914c84ba2d336cf58c0767dc35b3c58c
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2025-03-29 00:55:41 +0530

    added embedded systems project prototype

--- src/routes/departments/embedded_systems/+page.svelte
+++ src/routes/departments/embedded_systems/+page.svelte
@@ -4,17 +4,79 @@
 	let { data }: { data: PageData } = $props();
 </script>
 
+<input type="checkbox" id="my_modal_6" class="modal-toggle" />
+<div class="modal" role="dialog">
+	<div class="modal-box">
+		<h3 class="text-lg font-bold">Hello!</h3>
+		<p class="py-4">This modal works with a hidden checkbox!</p>
+		<div class="modal-action">
+			<label for="my_modal_6" class="btn">Close!</label>
+		</div>
+	</div>
+</div>
+
 <main
-	class="dotted-background-wrapper text-purple flex h-screen flex-col items-center justify-center !bg-[#F1B81A]"
+	class="dotted-background-wrapper text-purple flex min-h-screen flex-col items-center justify-center !bg-[#F1B81A]"
 >
 	<div class="flex h-full w-full flex-col">
-		<div class="border-purple flex h-[20%] flex-col items-center justify-center p-32">
+		<div class="flex h-[20%] flex-col items-center justify-center pt-28 pb-12">
 			<h1
 				class="es-card-title text-purple font-bebas text-9xl"
 				style:--es-title-text="es-title-text"
 			>
 				Embedded Systems
 			</h1>
+		</div>
+
+		<div class="flex flex-col items-center">
+			<div class="flex flex-col items-center gap-y-2">
+				<h2 class="font-pixelify text-3xl font-bold">Project Head</h2>
+				<div class="avatar">
+					<div class="ring-purple ring-offset-es-primary w-24 rounded-full ring-4 ring-offset-2">
+						<img
+							src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+							alt="es_dept_head"
+						/>
+					</div>
+				</div>
+				<p class="font-inria-sans text-2xl font-bold">Sumedh Prabhudeshai</p>
+			</div>
+		</div>
+
+		<div class="flex h-screen flex-col gap-y-2">
+			<h1 class="font-bebas pt-12 pb-0 pl-12 text-6xl">Our Projects</h1>
+			<div class="flex w-full grow flex-row items-center justify-center gap-x-4">
+				<div class="card bg-es-accent h-[60%] w-[40%]">
+					<div class="card-body">
+						<h2 class="card-title text-3xl">
+							Efficient Thermal Management System for Small Satellites
+						</h2>
+						<p>
+							A card component has a figure, a body part, and inside body there are title and
+							actions parts
+						</p>
+						<div class="card-actions justify-end">
+							<!-- <button class="btn bg-es-primary border-none shadow-none">Read More!</button> -->
+							<label for="my_modal_6" class="btn bg-es-primary border-none shadow-none">Read More</label>
+						</div>
+					</div>
+				</div>
+
+				<div class="card bg-es-accent h-[60%] w-[40%]">
+					<div class="card-body">
+						<h2 class="card-title text-3xl">
+							Hardware-Assisted Encryption for Resource-Constrained Devices
+						</h2>
+						<p>
+							A card component has a figure, a body part, and inside body there are title and
+							actions parts
+						</p>
+						<div class="card-actions justify-end">
+							<button class="btn bg-es-primary border-none shadow-none">Read More!</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </main>
